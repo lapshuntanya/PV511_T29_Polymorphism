99ae4cc043835ad7a84624baf61b1563c799516d
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-28 19:00:15 +0300

    class Employee

---
+++ Employee.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tetiana Perederii on 28.04.2026.
+//
+
+#include "Employee.h"
---
+++ Employee.h
@@ -0,0 +1,29 @@
+//
+// Created by Tetiana Perederii on 28.04.2026.
+//
+
+#ifndef PV511_T29_POLYMORPHISM_EMPLOYEE_H
+#define PV511_T29_POLYMORPHISM_EMPLOYEE_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Employee {
+protected:
+    string name;
+    float salary;
+public:
+    Employee();
+    Employee(string name, float salary);
+
+    void setName(string name);
+    void setSalary(float salary);
+    string getName()const;
+    float getSalary()const;
+
+    void showInfo()const;
+    float calcSalary()const;
+};
+
+
+#endif //PV511_T29_POLYMORPHISM_EMPLOYEE_H
