62851ad293da657b3c7948f96fa631403c701d9c
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-28 19:11:15 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-
+#include"Employee.h"
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main() {
 
+    Employee Diana("Diana", 15'000);
+    Diana.showInfo();
+    cout << "Total: " << Diana.calcSalary() << endl;
 
+    
     return 0;
 }
